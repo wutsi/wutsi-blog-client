cefb52485b11cf937ebf16d52f60123c9c7c061d
htchepannou <htchepannou@expedia.com>
2020-03-19 13:22:32 -0400

    make refeshToken nulable

--- src/main/kotlin/com/wutsi/blog/client/user/SessionDto.kt
+++ src/main/kotlin/com/wutsi/blog/client/user/SessionDto.kt
@@ -4,7 +4,7 @@ import java.util.Date
 
 data class SessionDto(
         val accessToken: String = "",
-        val refreshToken: String = "",
+        val refreshToken: String? = null,
         val accountId: Long = -1,
         val user: UserDto = UserDto(),
         val loginDateTime: Date = Date(),
