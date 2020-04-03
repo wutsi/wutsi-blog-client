52c198b6a39f887bf1218908a04ed978e3ef88a4
htchepannou <htchepannou@expedia.com>
2020-04-03 09:45:14 -0400

    move error DTO classes to wutsi-core

--- src/main/kotlin/com/wutsi/blog/client/error/ErrorDto.kt
+++
@@ -1,8 +0,0 @@
-package com.wutsi.blog.client.error
-
-data class ErrorDto (
-        val code:String = "",
-        val message:String = "",
-        val exception:String? = null,
-        val stackTrace:List<String>? = null
-)
--- src/main/kotlin/com/wutsi/blog/client/error/ErrorResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.wutsi.blog.client.error
-
-data class ErrorResponse(
-        val error: ErrorDto = ErrorDto()
-)
