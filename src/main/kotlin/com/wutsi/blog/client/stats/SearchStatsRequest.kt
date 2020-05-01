c65600c1e34ef62f57865c6316c206ac478eb42e
htchepannou <htchepannou@expedia.com>
2020-05-01 14:52:45 -0400

    change stats schemas

--- src/main/kotlin/com/wutsi/blog/client/stats/SearchStatsRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/stats/SearchStatsRequest.kt
@@ -4,7 +4,7 @@ import java.util.Date
 import javax.validation.constraints.NotNull
 
 data class SearchStatsRequest(
-        @get:NotNull val storyId: Long? = null,
+        @get:NotNull val targetId: Long? = null,
         @get:NotNull val startDate: Date? = null,
         @get:NotNull val endDate: Date? = null,
         @get:NotNull val type: StatsType? = null
--- src/main/kotlin/com/wutsi/blog/client/stats/StatsDto.kt
+++ src/main/kotlin/com/wutsi/blog/client/stats/StatsDto.kt
@@ -4,7 +4,7 @@ import java.util.Date
 
 data class StatsDto(
         val type: StatsType = StatsType.viewers,
-        val storyId: Long = -1,
+        val targetId: Long = -1,
         val date: Date = Date(),
         val value: Long = 0
 )
