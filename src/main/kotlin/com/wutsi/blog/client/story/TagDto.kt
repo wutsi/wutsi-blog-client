648a955d1b01e7c33b1e756b4f223767ffd7910d
htchepannou <htchepannou@expedia.com>
2020-05-04 15:20:04 -0400

    add display name

--- src/main/kotlin/com/wutsi/blog/client/story/TagDto.kt
+++ src/main/kotlin/com/wutsi/blog/client/story/TagDto.kt
@@ -3,5 +3,6 @@ package com.wutsi.blog.client.story
 data class TagDto(
         val id: Long = -1,
         val name: String = "",
+        val displayName: String = "",
         val totalStories: Long = 0
 )
