33b64b22e66a140a7c5a033167ced90312137a79
htchepannou <htchepannou@expedia.com>
2020-05-06 12:31:08 -0400

    add sort order

--- src/main/kotlin/com/wutsi/blog/client/story/SearchStoryRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/story/SearchStoryRequest.kt
@@ -1,10 +1,14 @@
 package com.wutsi.blog.client.story
 
+import com.wutsi.blog.client.SortOrder
+
+
 data class SearchStoryRequest(
         val userId: Long? = null,
         val status: StoryStatus? = null,
         val live: Boolean? = null,
         val limit: Int = 20,
         val offset: Int = 0,
-        val sortBy: StorySortStrategy = StorySortStrategy.modified
+        val sortBy: StorySortStrategy = StorySortStrategy.modified,
+        val sortOrder: SortOrder = SortOrder.descending
 )
