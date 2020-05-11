99940925322d08c248d6aaa4d90c76b1af82ef4b
htchepannou <htchepannou@expedia.com>
2020-05-11 09:12:00 -0400

    search by published date

--- src/main/kotlin/com/wutsi/blog/client/story/SearchStoryRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/story/SearchStoryRequest.kt
@@ -1,12 +1,15 @@
 package com.wutsi.blog.client.story
 
 import com.wutsi.blog.client.SortOrder
+import java.util.Date
 
 
 data class SearchStoryRequest(
         val userId: Long? = null,
         val status: StoryStatus? = null,
         val live: Boolean? = null,
+        val publishedStartDate: Date? = null,
+        val publishedEndDate: Date? = null,
         val limit: Int = 20,
         val offset: Int = 0,
         val sortBy: StorySortStrategy = StorySortStrategy.modified,
