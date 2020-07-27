0e4aabf88575c372c0734800ca0bb11da465b299
willy kouagnia <willkoua@gmail.com>
2020-07-28 00:29:14 +0100

    add blog parameter to SearchUserRequest

--- src/main/kotlin/com/wutsi/blog/client/user/SearchUserRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/user/SearchUserRequest.kt
@@ -3,5 +3,6 @@ package com.wutsi.blog.client.user
 data class SearchUserRequest(
         val userIds: List<Long> = emptyList(),
         val limit: Int = 20,
-        val offset: Int = 0
+        val offset: Int = 0,
+        val blog: Boolean? = null
 )
