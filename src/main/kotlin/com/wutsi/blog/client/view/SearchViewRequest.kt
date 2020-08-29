a9b279a7ae7e80ac7afa44288f6ecd48b3cbf84f
htchepannou <htchepannou@expedia.com>
2020-08-28 21:00:03 -0400

    add deviceId into SearchViewRequest

--- src/main/kotlin/com/wutsi/blog/client/view/SearchViewRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/view/SearchViewRequest.kt
@@ -6,5 +6,6 @@ import javax.validation.constraints.NotNull
 
 data class SearchViewRequest(
         @get:NotNull @get:NotEmpty val storyIds: List<Long> = emptyList(),
-        val userId: Long? = null
+        val userId: Long? = null,
+        val deviceId: String? = null
 )
