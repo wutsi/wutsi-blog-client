f2f64edbe4d5cc60e5a3adcdd0596cd49eb06c8b
htchepannou <htchepannou@expedia.com>
2020-09-12 10:45:56 -0400

    remove view threshold

--- src/main/kotlin/com/wutsi/blog/client/story/RecommendStoryRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/story/RecommendStoryRequest.kt
@@ -7,6 +7,5 @@ data class RecommendStoryRequest(
         @get:NotNull val storyId: Long? = null,
         val userId: Long? = null,
         val language: String? = null,
-        val minViewers: Int? = null,
         val limit: Int = 20
 )
