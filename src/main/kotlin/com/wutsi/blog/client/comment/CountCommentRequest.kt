d4de2a0d5fa848554a22a8c03d1ff79260677301
htchepannou <htchepannou@expedia.com>
2020-10-07 19:41:23 -0400

    search api

--- src/main/kotlin/com/wutsi/blog/client/comment/CountCommentRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.wutsi.blog.client.comment
-
-import javax.validation.constraints.NotEmpty
-
-data class CountCommentRequest(
-        @get:NotEmpty() val storyIds: List<Long> = emptyList()
-)
