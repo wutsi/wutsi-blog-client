11cf886184c2a882219176c913300ed0adcaa9c2
htchepannou <htchepannou@expedia.com>
2020-10-07 09:02:28 -0400

    improve like count

--- src/main/kotlin/com/wutsi/blog/client/like/CountLikeRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.wutsi.blog.client.like
-
-import javax.validation.constraints.NotEmpty
-
-data class CountLikeRequest(
-        @get:NotEmpty() val storyIds: List<Long> = emptyList()
-)
--- src/main/kotlin/com/wutsi/blog/client/like/SearchLikeRequest.kt
+++ src/main/kotlin/com/wutsi/blog/client/like/SearchLikeRequest.kt
@@ -1,6 +1,12 @@
 package com.wutsi.blog.client.like
 
+import java.util.Date
+
 data class SearchLikeRequest(
         val storyIds: List<Long> = emptyList(),
-        val userId: Long = -1
+        val userId: Long? = null,
+        val authorId: Long? = null,
+        val since: Date? = null,
+        val limit: Int = 20,
+        val offset: Int = 0
 )
