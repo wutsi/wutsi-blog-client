49343c03c471096b2d3c77ccf997200575ee8e60
htchepannou <htchepannou@expedia.com>
2020-10-18 08:43:50 -0400

    viewed ids

--- src/main/kotlin/com/wutsi/blog/client/story/SortStoryResponse.kt
+++ src/main/kotlin/com/wutsi/blog/client/story/SortStoryResponse.kt
@@ -2,5 +2,6 @@ package com.wutsi.blog.client.story
 
 
 data class SortStoryResponse(
-        val storyIds: List<Long> = emptyList()
+        val storyIds: List<Long> = emptyList(),
+        val viewedStoryIds: List<Long> = emptyList()
 )
