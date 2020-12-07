beb567a0ce4b74ea6e0f0c7a374104996078df89
htchepannou <htchepannou@expedia.com>
2020-12-07 05:38:26 -0500

    update

--- src/main/kotlin/com/wutsi/blog/sdk/impl/PinApiImpl.kt
+++ src/main/kotlin/com/wutsi/blog/sdk/impl/PinApiImpl.kt
@@ -3,25 +3,26 @@ package com.wutsi.blog.sdk.impl
 import com.wutsi.blog.client.pin.CreatePinRequest
 import com.wutsi.blog.client.pin.CreatePinResponse
 import com.wutsi.blog.client.pin.SearchPinResponse
+import com.wutsi.blog.client.user.SessionDto
 import com.wutsi.blog.sdk.PinApi
 import com.wutsi.blog.sdk.WutsiEnvironment
 import com.wutsi.core.http.Http
 
 class PinApiImpl(
         private val http: Http,
+        private val session: SessionDto,
         private val environment: WutsiEnvironment
 ): PinApi {
-    override fun search(userId: Long, limit: Int, offset: Int): SearchPinResponse {
-        val path = "?limit=$limit&offset=$offset&userId=$userId"
-        return return http.get(uri(path), SearchPinResponse::class.java).body!!
+    override fun search(): SearchPinResponse {
+        return return http.get(uri(), SearchPinResponse::class.java).body!!
     }
 
     override fun create(request: CreatePinRequest): CreatePinResponse =
         http.post(uri(), request, CreatePinResponse::class.java).body!!
 
-    override fun delete(id: Long) =
-        http.delete(uri("/$id"))
+    override fun delete() =
+        http.delete(uri())
 
-    private fun uri(path: String = ""): String =
-        "${environment.apiUrl}/v1/pins$path"
+    private fun uri(): String =
+        "${environment.apiUrl}/v1/users/${session.userId}/pin"
 }
