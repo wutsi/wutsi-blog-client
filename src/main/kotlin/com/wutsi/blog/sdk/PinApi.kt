8c2990e7203481e978527b183f3a4ce6dd75fb41
htchepannou <htchepannou@expedia.com>
2020-12-07 05:38:49 -0500

    update api

--- src/main/kotlin/com/wutsi/blog/sdk/PinApi.kt
+++ src/main/kotlin/com/wutsi/blog/sdk/PinApi.kt
@@ -5,7 +5,7 @@ import com.wutsi.blog.client.pin.CreatePinResponse
 import com.wutsi.blog.client.pin.SearchPinResponse
 
 interface PinApi {
-    fun search(userId: Long, limit: Int = 20, offset: Int = 0): SearchPinResponse
+    fun search(limit: Int = 20, offset: Int = 0): SearchPinResponse
     fun create(request: CreatePinRequest): CreatePinResponse
-    fun delete(id: Long)
+    fun delete()
 }
