8aaa6e262a155a0ccd46c2bb29252db63d2534d5
htchepannou <htchepannou@expedia.com>
2021-03-29 15:24:02 -0400

    change versioning strategy of events

--- src/main/kotlin/com/wutsi/blog/stream/EventType.kt
+++ src/main/kotlin/com/wutsi/blog/stream/EventType.kt
@@ -3,6 +3,6 @@ package com.wutsi.blog.stream
 enum class EventType(
     val urn: String
 ) {
-    LIKED("urn:wutsi:1.0:like:liked"),
-    UNLIKED("urn:wutsi:1.0:like:unliked"),
+    LIKED("urn:wutsi:like:1.0:liked"),
+    UNLIKED("urn:wutsi:like:1.0:unliked"),
 }
